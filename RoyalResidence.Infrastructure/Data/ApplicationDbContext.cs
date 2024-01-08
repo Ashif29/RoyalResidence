080539e412c98b3cf384b152da40630b59f46972
ashif29 <asifcr75@gmail.com>
2024-01-08 11:01:20 -0800

    Created Clean Architecture Setup, Added ApplicationDbContext, and Dependency Injection. Manages NuGetPaceges, Created Database and villa table.

---
+++ RoyalResidence.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoyalResidence.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalResidence.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options) :base(options)
+        {
+            
+        }
+        public DbSet<Villa> Villas  { get; set; }
+    }
+}
