323e699aa92211d8ec2ba783c51bbd28572d20a4
ashif29 <asifcr75@gmail.com>
2024-01-08 22:39:01 -0800

    Added View For Villa. Added Bootstrap icon for create new villa. displayed villa list on UL. Added bootstrap CDN for JS & CSS (version 5.3.1) Applyed Bootstrap Dark theme.

--- RoyalResidence.Infrastructure/Migrations/20240109040420_SeedVillasTabletoDb.Designer.cs
+++ RoyalResidence.Infrastructure/Migrations/20240109040420_SeedVillasTabletoDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RoyalResidence.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using RoyalResidence.Infrastructure.Data;
 namespace RoyalResidence.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240109040420_SeedVillasTabletoDb")]
+    partial class SeedVillasTabletoDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RoyalResidence.Web/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoyalResidence.Infrastructure.Data;
+
+namespace RoyalResidence.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
