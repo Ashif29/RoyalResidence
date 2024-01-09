21643e09abe5f6a8ab0bd4160b319bc139755043
ashif29 <asifcr75@gmail.com>
2024-01-08 23:03:56 -0800

    Created A View for Create A Villa with UI

--- RoyalResidence.Web/Controllers/VillaController.cs
+++ RoyalResidence.Web/Controllers/VillaController.cs
@@ -16,5 +16,9 @@ namespace RoyalResidence.Web.Controllers
             var villas = _db.Villas.ToList();
             return View(villas);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
