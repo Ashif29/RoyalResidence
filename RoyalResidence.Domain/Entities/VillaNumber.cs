5eb7654c8d8ccad5f6a160c570df0ef436125470
ashif29 <asifcr75@gmail.com>
2024-01-09 23:44:16 -0800

    Added VillaNumber Table which has Foregin key Relation to Villa table , and seed with pre defined data

---
+++ RoyalResidence.Domain/Entities/VillaNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalResidence.Domain.Entities
+{
+    public class VillaNumber
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [DisplayName("Villa Number")]
+        public int Villa_Number { get; set; }
+        [ForeignKey("Villa")]
+        [DisplayName("Villa Id")]
+        public int VillaId { get; set; } 
+        public Villa Villa { get; set; }
+        [DisplayName("Special Details")]
+        public string? SpecilaDetails { get; set; }
+    }
+}
--- RoyalResidence.Infrastructure/Data/ApplicationDbContext.cs
+++ RoyalResidence.Infrastructure/Data/ApplicationDbContext.cs
@@ -15,7 +15,7 @@ namespace RoyalResidence.Infrastructure.Data
             
         }
         public DbSet<Villa> Villas  { get; set; }
-
+        public DbSet<VillaNumber> VillaNumbers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //    base.OnModelCreating(modelBuilder);
@@ -31,26 +31,74 @@ namespace RoyalResidence.Infrastructure.Data
                     Price = 200,
                     Sqft = 550,
                 },
-                    new Villa
-                    {
-                        Id = 2,
-                        Name = "Premium Pool Villa",
-                        Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                        ImageUrl = "https://placehold.co/600x401",
-                        Occupancy = 4,
-                        Price = 300,
-                        Sqft = 550,
-                    },
-                    new Villa
-                    {
-                        Id = 3,
-                        Name = "Luxury Pool Villa",
-                        Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                        ImageUrl = "https://placehold.co/600x402",
-                        Occupancy = 4,
-                        Price = 400,
-                        Sqft = 750,
-                    });
+                new Villa
+                {
+                    Id = 2,
+                    Name = "Premium Pool Villa",
+                    Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x401",
+                    Occupancy = 4,
+                    Price = 300,
+                    Sqft = 550,
+                },
+                new Villa
+                {
+                    Id = 3,
+                    Name = "Luxury Pool Villa",
+                    Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x402",
+                    Occupancy = 4,
+                    Price = 400,
+                    Sqft = 750,
+                }
+            );
+            modelBuilder.Entity<VillaNumber>().HasData(
+                new VillaNumber
+                {
+                    Villa_Number = 101,
+                    VillaId = 1,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 102,
+                    VillaId = 1,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 103,
+                    VillaId = 1,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 201,
+                    VillaId = 2,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 202,
+                    VillaId = 2,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 203,
+                    VillaId = 2,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 301,
+                    VillaId = 3,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 302,
+                    VillaId = 3,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 401,
+                    VillaId = 4,
+                }
+            );
         }
     }
 }
