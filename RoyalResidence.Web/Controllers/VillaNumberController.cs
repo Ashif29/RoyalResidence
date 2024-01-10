822fad12f39520ff4ec581be94a5d50b90f9bafd
ashif29 <asifcr75@gmail.com>
2024-01-09 23:46:42 -0800

    Added Villa Number Controller

---
+++ RoyalResidence.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoyalResidence.Domain.Entities;
+using RoyalResidence.Infrastructure.Data;
+
+namespace RoyalResidence.Web.Controllers
+{
+    public class VillaNumber : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaNumber(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
