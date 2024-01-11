931610491e29a1576df36000413236d8581b8ea4
ashif29 <asifcr75@gmail.com>
2024-01-11 09:09:03 -0800

    Added Generic Repository

--- RoyalResidence.Infrastructure/Repository/VillaRepository.cs
+++ RoyalResidence.Infrastructure/Repository/VillaRepository.cs
@@ -11,63 +11,21 @@ using System.Threading.Tasks;
 
 namespace RoyalResidence.Infrastructure.Repository
 {
-    public class VillaRepository : IVillaRepository
+    public class VillaRepository : Repository<Villa>, IVillaRepository
     {
         private readonly ApplicationDbContext _db;
 
-        public VillaRepository(ApplicationDbContext db)
+        public VillaRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
-        }
-        public void Add(Villa entity)
-        {
-            _db.Villas.Add(entity);
         }
         public void Update(Villa entity)
         {
             _db.Villas.Update(entity);
         }
-        public void Remove(Villa entity)
-        {
-            _db.Villas.Remove(entity);
-        }
         public void Save()
         {
             _db.SaveChanges();
-        }
-        public Villa Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _db.Set<Villa>();
-            query = query.Where(filter);
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                //Villa,VillaNumber -- case sensitive
-                foreach (var includeProp in includeProperties
-                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    query = query.Include(includeProp);
-                }
-            }
-            return query.FirstOrDefault();
-        }
-
-        public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _db.Set<Villa>();
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                //Villa,VillaNumber -- case sensitive
-                foreach (var includeProp in includeProperties
-                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    query = query.Include(includeProp);
-                }
-            }
-            return query.ToList();
         }
     }
 }
