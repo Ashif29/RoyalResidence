d90d0a911ed161d172e0e0a9ece5d93240ed9500
ashif29 <asifcr75@gmail.com>
2024-01-12 21:03:04 -0800

    Account Controller

---
+++ RoyalResidence.Web/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using RoyalResidence.Application.Common.Interfaces;
+using RoyalResidence.Domain.Entities;
+
+namespace RoyalResidence.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager   <ApplicationUser> _userManager;
+        private readonly SignInManager <ApplicationUser> _signInManager;
+        private readonly RoleManager   <ApplicationUser> _roleManager;
+
+        public AccountController
+            (IUnitOfWork unitOfWork,
+             UserManager   <ApplicationUser> userManager,
+             SignInManager <ApplicationUser> signInManager, 
+             RoleManager   <ApplicationUser> roleManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
