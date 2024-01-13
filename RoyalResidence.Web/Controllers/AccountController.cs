95af804bf1dc7dd5808ffbdc62a24723cc4a0bbd
ashif29 <asifcr75@gmail.com>
2024-01-12 22:01:50 -0800

    Login And Registration View and Add roles to table

--- RoyalResidence.Web/Controllers/AccountController.cs
+++ RoyalResidence.Web/Controllers/AccountController.cs
@@ -10,15 +10,14 @@ namespace RoyalResidence.Web.Controllers
     public class AccountController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly UserManager   <ApplicationUser> _userManager;
-        private readonly SignInManager <ApplicationUser> _signInManager;
-        private readonly RoleManager   <ApplicationUser> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController
-            (IUnitOfWork unitOfWork,
-             UserManager   <ApplicationUser> userManager,
-             SignInManager <ApplicationUser> signInManager, 
-             RoleManager   <ApplicationUser> roleManager)
+        public AccountController(IUnitOfWork unitOfWork,
+             UserManager<ApplicationUser> userManager,
+             SignInManager<ApplicationUser> signInManager, 
+             RoleManager<IdentityRole> roleManager)
         {
             _unitOfWork = unitOfWork;
             _userManager = userManager;
@@ -37,6 +36,11 @@ namespace RoyalResidence.Web.Controllers
         }
         public IActionResult Register()
         {
+            if (!_roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
+                _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
+            }
             return View();
         }
     }
